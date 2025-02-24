3017434c78779a2a54d7e2113d7ca71e2f46b7b5
Charlos Pardomuan Purba <136062290+charlospurba@users.noreply.github.com>
2025-02-24 13:41:31 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,8 +7,8 @@ from sklearn.cluster import KMeans
 ss.text("Nama: Charlos Pardomuan Purba")
 ss.title('Data Wrangling')
 
-customers_df = pd.read_csv("../data/olist_customers_dataset.csv", delimiter=",") 
-geolocation_df = pd.read_csv("../data/olist_geolocation_dataset.csv", delimiter=",")
+customers_df = pd.read_csv("data/olist_customers_dataset.csv", delimiter=",") 
+geolocation_df = pd.read_csv("data/olist_geolocation_dataset.csv", delimiter=",")
 
 # Filtering: User can select a province for analysis
 selected_state = ss.selectbox("Pilih Provinsi untuk Filter", customers_df['customer_state'].unique())
